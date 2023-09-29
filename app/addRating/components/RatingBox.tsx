10ed88334a2a2f8055c2bbb264ab9eefbe7931cf
Saransh Gangwar <saransh7919@gmail.com>
2023-09-29 14:09:24 +0530

    updated form

--- app/addRating/components/RatingBox.tsx
+++ app/addRating/components/RatingBox.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import styles from "./ratingBox.module.css";
 import { TextField } from "@mui/material";
 import React, { useState } from "react";
@@ -12,7 +12,7 @@ const RatingBox: React.FC<Iprops> = ({ data }) => {
   const [rating, setRating] = useState(0);
   const [isSubmit, setIsSubmit] = useState<boolean>(false);
   const [suggestion, setSuggestion] = useState("");
-  const [error, setError]= useState<string>("");
+  const [error, setError] = useState<string>("");
 
   const [formData, setFormData] = useState({
     Experience: "",
@@ -31,16 +31,18 @@ const RatingBox: React.FC<Iprops> = ({ data }) => {
   const handleStarClick = (clickedRating: number) => {
     setRating(clickedRating);
   };
-  
-  const handleSubmit = () => {
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
     postReview(reviewData)
       .then((res) => {
-          setIsSubmit(true);
-        })
-        .catch((err) => {
-            console.error("Error while posting review: ", err);
-            setError(err);
-            alert(err);
+        setIsSubmit(true);
+      })
+      .catch((err) => {
+        console.error("Error while posting review: ", err);
+        setError(err);
+        alert(err);
       });
   };
 
@@ -57,168 +59,180 @@ const RatingBox: React.FC<Iprops> = ({ data }) => {
   return (
     <div className={styles.frameParent}>
       <div className={styles.frameGroup}>
-        {!isSubmit && (
-          <div className={styles.frameDiv}>
-            <div className={styles.rateYourExperienceWithConParent}>
-              <div className={styles.rateYourExperience1}>
-                Rate your experience with “Contractor Company Name”
-              </div>
-              <div className={styles.starContainer}>
-                <div className={styles.interfaceStarParent}>
-                  {Array.from({ length: 5 }, (_, index) => (
-                    <span
-                      key={index}
-                      onClick={() => handleStarClick(index + 1)}
-                      style={{ cursor: "pointer" }}
-                    >
-                      {index < rating ? (
-                        <img
-                          src="/assets/StarFilled.png"
-                          alt=""
-                          className={styles.star}
-                        />
-                      ) : (
-                        <img
-                          src="/assets/Star.png"
-                          alt=""
-                          className={styles.star}
-                        />
-                      )}
-                    </span>
-                  ))}
+        <form onSubmit={handleSubmit}>
+          {!isSubmit && (
+            <div className={styles.frameDiv}>
+              <div className={styles.rateYourExperienceWithConParent}>
+                <div className={styles.rateYourExperience1}>
+                  Rate your experience with “Contractor Company Name”
                 </div>
-                <div className={styles.veryBadParent}>
-                  <div className={styles.veryBad}>Very bad</div>
-                  <div className={styles.veryBad}>Very good</div>
+                <div className={styles.starContainer}>
+                  <div className={styles.interfaceStarParent}>
+                    {Array.from({ length: 5 }, (_, index) => (
+                      <span
+                        key={index}
+                        onClick={() => handleStarClick(index + 1)}
+                        style={{ cursor: "pointer" }}
+                      >
+                        {index < rating ? (
+                          <img
+                            src="/assets/StarFilled.png"
+                            alt=""
+                            className={styles.star}
+                          />
+                        ) : (
+                          <img
+                            src="/assets/Star.png"
+                            alt=""
+                            className={styles.star}
+                          />
+                        )}
+                      </span>
+                    ))}
+                  </div>
+                  <div className={styles.veryBadParent}>
+                    <div className={styles.veryBad}>Very bad</div>
+                    <div className={styles.veryBad}>Very good</div>
+                  </div>
                 </div>
               </div>
-            </div>
 
-            {rating < 5 && rating != 0 && (
+              {rating < 5 && rating != 0 && (
+                <div className={styles.howWasYourOverallExperiencParent}>
+                  <div className={styles.suggestions}>
+                    <div className={styles.howWasYour1}>
+                      What could have been better?
+                    </div>
+                    <div className={styles.itemsWrapper}>
+                      <div className={styles.item}>
+                        <input
+                          type="radio"
+                          id="Safety"
+                          name="categories"
+                          value="Safety"
+                          onClick={handleClick}
+                        />
+                        <label htmlFor="Safety">Safety</label>
+                      </div>
+                      <div className={styles.item}>
+                        <input
+                          type="radio"
+                          id="Communication"
+                          name="categories"
+                          value="Communication"
+                          onClick={handleClick}
+                        />
+                        <label htmlFor="Communication">Communication</label>
+                      </div>
+                      <div className={styles.item}>
+                        <input
+                          type="radio"
+                          id="Food"
+                          name="categories"
+                          value="Food and Stay"
+                          onClick={handleClick}
+                        />
+                        <label htmlFor="Food">Food and Stay</label>
+                      </div>
+                      <div className={styles.item}>
+                        <input
+                          type="radio"
+                          id="Professionalism"
+                          name="categories"
+                          value="Professionalism"
+                          onClick={handleClick}
+                        />
+                        <label htmlFor="Professionalism">Professionalism</label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
               <div className={styles.howWasYourOverallExperiencParent}>
-                <div className={styles.suggestions}>
+                <div className={styles.experience}>
                   <div className={styles.howWasYour1}>
-                    What could have been better?
+                    How was your overall experience ? (100% Secure)
                   </div>
-                  <div className={styles.itemsWrapper}>
-                    <div className={styles.item}>
-                      <input
-                        type="radio"
-                        id="Safety"
-                        name="categories"
-                        value="Safety"
-                        onClick={handleClick}
-                      />
-                      <label htmlFor="Safety">Safety</label>
-                    </div>
-                    <div className={styles.item}>
-                      <input
-                        type="radio"
-                        id="Communication"
-                        name="categories"
-                        value="Communication"
-                        onClick={handleClick}
-                      />
-                      <label htmlFor="Communication">Communication</label>
-                    </div>
-                    <div className={styles.item}>
-                      <input
-                        type="radio"
-                        id="Food"
-                        name="categories"
-                        value="Food and Stay"
-                        onClick={handleClick}
-                      />
-                      <label htmlFor="Food">Food and Stay</label>
-                    </div>
-                    <div className={styles.item}>
-                      <input
-                        type="radio"
-                        id="Professionalism"
-                        name="categories"
-                        value="Professionalism"
-                        onClick={handleClick}
-                      />
-                      <label htmlFor="Professionalism">Professionalism</label>
-                    </div>
+                  <div className={styles.sl12202147430151Parent}>
+                    <img
+                      className={styles.sl12202147430151Icon}
+                      alt=""
+                      src="/assets/sl_122021_47430_15 1.png"
+                    />
+                    <b className={styles.label}>100% SECURE</b>
                   </div>
                 </div>
+                <TextField
+                  required
+                  id="outlined-required"
+                  label="Required"
+                  placeholder="Kindly share details about their payment speed, paper work,  professionalism etc"
+                  multiline
+                  fullWidth
+                  rows={4}
+                  name="Experience"
+                  value={formData.Experience}
+                  onChange={handleChange}
+                />
               </div>
-            )}
-            <div className={styles.howWasYourOverallExperiencParent}>
-              <div className={styles.experience}>
-                <div className={styles.howWasYour1}>
-                  How was your overall experience ? (100% Secure)
+              <div className={styles.howWasYourOverallExperiencParent}>
+                <div className={styles.experience}>
+                  <div className={styles.howWasYour1}>
+                    Your Name (we will never share your name)
+                  </div>
                 </div>
-                <div className={styles.sl12202147430151Parent}>
-                  <img
-                    className={styles.sl12202147430151Icon}
-                    alt=""
-                    src="/assets/sl_122021_47430_15 1.png"
-                  />
-                  <b className={styles.label}>100% SECURE</b>
-                </div>
+                <TextField
+                  required
+                  id="outlined-required"
+                  label="Required"
+                  placeholder="Enter your name"
+                  fullWidth
+                  name="Name"
+                  value={formData.Name}
+                  onChange={handleChange}
+                />
               </div>
-              <TextField
-                id="outlined-textarea"
-                placeholder="Kindly share details about their payment speed, paper work,  professionalism etc"
-                multiline
-                fullWidth
-                rows={4}
-                required
-                name="Experience"
-                value={formData.Experience}
-                onChange={handleChange}
-              />
-            </div>
-            <div className={styles.howWasYourOverallExperiencParent}>
-              <div className={styles.experience}>
-                <div className={styles.howWasYour1}>
-                  Your Name (we will never share your name)
+              <div className={styles.howWasYourOverallExperiencParent}>
+                <div className={styles.experience}>
+                  <div className={styles.howWasYour1}>
+                    Your Phone No. (we will never share your number)
+                  </div>
+                  <div className={styles.sl12202147430151Parent}>
+                    <img
+                      className={styles.sl12202147430151Icon}
+                      alt=""
+                      src="/assets/sl_122021_47430_15 1.png"
+                    />
+                    <b className={styles.label}>100% SECURE</b>
+                  </div>
                 </div>
-              </div>
-              <TextField
-                id="outlined-textarea"
-                placeholder="Enter your name"
-                fullWidth
-                required
-                name="Name"
-                value={formData.Name}
-                onChange={handleChange}
-              />
-            </div>
-            <div className={styles.howWasYourOverallExperiencParent}>
-              <div className={styles.experience}>
-                <div className={styles.howWasYour1}>
-                  Your Phone No. (we will never share your number)
-                </div>
-                <div className={styles.sl12202147430151Parent}>
-                  <img
-                    className={styles.sl12202147430151Icon}
-                    alt=""
-                    src="/assets/sl_122021_47430_15 1.png"
-                  />
-                  <b className={styles.label}>100% SECURE</b>
-                </div>
-              </div>
 
-              <TextField
-                id="textarea"
-                placeholder="9999999999"
-                fullWidth
-                required
-                name="PhoneNumber"
-                value={formData.PhoneNumber}
-                onChange={handleChange}
-              />
-              {/* <button className={styles.verifyWrapper}>Verify</button> */}
+                <TextField
+                  required
+                  id="outlined-required"
+                  label="Required"
+                  placeholder="9898989898"
+                  fullWidth
+                  type="number"
+                  name="PhoneNumber"
+                  value={formData.PhoneNumber}
+                  onChange={handleChange}
+                />
+                {/* <button className={styles.verifyWrapper}>Verify</button> */}
+              </div>
             </div>
-          </div>
-        )}
+          )}
 
-        {/* <button className={styles.ctaWrapper}>Submit</button> */}
-        {!isSubmit && <button className={styles.ctaWrapper2}
-        onClick={handleSubmit}>Submit</button>}
+          {/* <button className={styles.ctaWrapper}>Submit</button> */}
+          {!isSubmit && (
+            <button
+              type="submit"
+              className={styles.ctaWrapper2}
+            >
+              Submit
+            </button>
+          )}
+        </form>
 
         {isSubmit && (
           <div className={styles.submitted_card}>
@@ -238,8 +252,10 @@ const RatingBox: React.FC<Iprops> = ({ data }) => {
                 </div>
               </div>
               <div className={styles.component}>
-                <button className={styles.ctaWrapper2}
-                onClick={()=> setIsSubmit(false)}>
+                <button
+                  className={styles.ctaWrapper2}
+                  onClick={() => setIsSubmit(false)}
+                >
                   Add another review
                 </button>
                 <div className={styles.gotohome}>
